15ccea8a7a6bd584f69ba03593598b4ed7792d9b
vikking <vikking>
2008-09-23 15:41:03 +0000

    Smarty.class.php is already included in smarty.handler.class.php

--- jetbird/index.php
+++ jetbird/index.php
@@ -22,7 +22,6 @@
 	require_once "include/functions.php";
 	$process_start = timer();		// use this wherever you want, can be useful for debugging
 	require_once "include/configuration.php";
-	require_once "include/smarty/Smarty.class.php";
 	require_once "include/database.connect.php";
 	require_once "include/smarty.handler.class.php";
 	
