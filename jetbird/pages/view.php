8d0743422791c653fd88619ab63d9e57c91ed989
vikking <vikking>
2008-09-24 18:47:48 +0000

    Cleaned this up some, why would we need to use switch here, I don't know if we don't need to view something else ^^

--- jetbird/pages/view.php
+++ jetbird/pages/view.php
@@ -15,62 +15,53 @@
 	    You should have received a copy of the GNU General Public License
 	    along with Jetbird.  If not, see <http://www.gnu.org/licenses/>.
 	*/
-		
-	switch($_GET['action']){
+
 	/*
-	/*	view section     
+	/*	the main post section, here the main post will be done.    
 	*/	
-		case view_post:
-		
-		/*
-		/*	the main post section, here the main post will be done.    
-		*/	
-		
-			if(!isset($_GET['post_id'])){
-				$smarty->assign('view', FALSE);
-			}
-			
-			$query = "SELECT post, title, date
-						FROM post
-						WHERE post_id = ". $_GET['post_id'] ."";			
-			
-			$result = $dbconnection->query($query);
-			if(mysql_num_rows($result) == 1){
-				$row = mysql_fetch_array($result);
-				$smarty->assign('view_post', nl2br($row['post']));
-				$smarty->assign('view_date', date("D M j G:i  Y", $row['date']));
-				$smarty->assign('view_title', $row['title']);
-			}else{
-				$smarty->assign('view', FALSE);
-			}
-			
-		/*
-		/*	the comments sections, here the comments will be done   
-		*/
-		
-			$query = "SELECT comment.comment_parent_id, comment.comment, comment.comment_date,
-								users.username, users.user_id, comment.comment_id, comment.pcomment_id
-								FROM comment, users
-								WHERE comment_parent_id = ". $_GET['post_id'] ."
-								AND comment.pcomment_id = users.user_id";
-		
-			$result = $dbconnection->query($query);
-		
-			while($row = mysql_fetch_array($result)){
-				$view_content['comment'][] = nl2br($row['comment']);
-				$view_content['date'][] = date("D M j G:i  Y", $row['comment_date']);
-				$view_content['username'][] = $row['username'];
-			}
-							
-			$smarty->assign('comment', $view_content['comment']);
-			$smarty->assign('date', $view_content['date']);
-			$smarty->assign('username', $view_content['username']);		
-		break;
-			
-		default:
-			$smarty->assign('view', FALSE);
+
+	$view_id = $_GET['id'];
+	if(!isset($_GET['id'])){
+		redirect("./");
 	}
-		
+
+	$query = "SELECT post, title, date
+		FROM post
+		WHERE post_id = ". $view_id ."";			
+
+	$result = $dbconnection->query($query);
+	if(mysql_num_rows($result) == 1){
+		$row = mysql_fetch_array($result);
+		$smarty->assign('view_post', nl2br($row['post']));
+		$smarty->assign('view_date', date($config['global']['timestamp'], $row['date']));
+		$smarty->assign('view_title', $row['title']);
+	}
+
+	/*
+	/*	the comments sections, here the comments will be done   
+	*/
+
+	$query = "SELECT comment.comment_parent_id, comment.comment, comment.comment_date,
+		users.username, users.user_id, comment.comment_id, comment.pcomment_id
+		FROM comment, users
+		WHERE comment_parent_id = ". $view_id ."
+		AND comment.pcomment_id = users.user_id";
+
+	$comments = $dbconnection->fetch_array($query);
+	
+	if(count($comments) != 0){
+		foreach($comments as $comment){
+			$view_content['comment'][] = nl2br($comment['comment']);
+			$view_content['date'][] = date($config['global']['timestamp'], $comment['comment_date']);
+			$view_content['username'][] = $comment['username'];
+		}
+	}
+
+	$smarty->assign('comment', $view_content['comment']);
+	$smarty->assign('date', $view_content['date']);
+	$smarty->assign('username', $view_content['username']);		
+
+
 	$smarty->assign('queries', $dbconnection->queries);	
 	$smarty->display('view.tpl');
 	
