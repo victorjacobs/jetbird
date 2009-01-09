33fce0b734e69a76e06e148a9124ed9d8128213a
Vikking <vikking@gmail.com>
2009-01-09 20:14:12 +0100

    Updates

--- jetbird/pages/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require_once "include/bbcode.class.php";
-	
-	$var = "Toen gingen de moeders richting [i]hun [url='http://apple.com']moeder[/url] enzo[/i] met pizza";
-	
-	$bbcode = new bbcode($var);
-
-?>
