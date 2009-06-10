c983238486e860fd6410448392d9e08efe7068ae
Vikking <vikking@gmail.com>
2009-06-10 11:01:37 +0200

    Rewrote the remember login function, uses database now. A lot safer.

--- jetbird/include/login.bootstrap.php
+++ jetbird/include/login.bootstrap.php
@@ -16,29 +16,56 @@
 	    along with Jetbird.  If not, see <http://www.gnu.org/licenses/>.
 	*/
 	
-	if(isset($_COOKIE['logged_in_as']) && ($_COOKIE['logged_in_as'] != $_SESSION['logged_in_as'])){		// Session expired, but we are still logged in by cookie
-		// Refresh the cookie's expire timer by setting new uniqid
-		$login_id = uniqid();
-		setcookie('logged_in_as', $login_id, time() + $config['global']['login_expire']);
-		setcookie('user_id', $_COOKIE['user_id'], time() + $config['global']['login_expire']);
-		$_SESSION['logged_in_as'] = $login_id;
+	if(isset($_COOKIE['login_key']) && !$_SESSION['login']){
+		echo "<b>DEBUG:</b> Bootstrapping login";
 		
-		// Re-set all other session vars, we assume here that $_COOKIE['user_id'] was a valid user id
-		//  since it was set using the login script
-		$query = "	SELECT *
-					FROM user
-					WHERE user_id = ". $_COOKIE['user_id'];
-		$user = $db->fetch_array($query);
+		// Let's check whether the login_key is valid
+		$session_info_query = $db->query("	SELECT *
+											FROM user_session
+											WHERE user_session_key = '". $_COOKIE['login_key'] ."'");
 		
-		$_SESSION['user_level'] = $user[0]['user_level'];
-		$_SESSION['user_name'] = $user[0]['user_name'];
-		$_SESSION['user_id'] = $user[0]['user_id'];
-		$_SESSION['login'] = true;
-		
-		// Since our session expired, we are essentially just logging in again
-		$db->query("	UPDATE user
-								SET user_last_login = ". time() ."
-								WHERE user_id = ". $user[0]['user_id']);
+		if($db->num_rows($session_info_query) == 1){
+			$session_info = $db->fetch_array($session_info_query);
+			
+			/* This next block of code assumes that the login can't expire while
+			 * browsing the site (because it the block is located under a !$_SESSION
+			 * statement). I don't know if this a good thing to do. But normally
+			 * it won't be a problem, only if someone changes $config['global']['login_expire']
+			 * while people are logged in. But then again, what difference will that make.
+			*/
+			if($session_info[0]['user_session_age'] + $config['global']['login_expire'] <= time()){
+				$db->query("DELETE FROM user_session
+							WHERE user_session_key = '". $_COOKIE['login_key'] ."'");
+				cookie_destroy("login_key");
+			}else{
+				// Refresh cookie with same login_key
+				setcookie('login_key', $_COOKIE['login_key'], time() + $config['global']['login_expire']);
+				
+				$user_info = $db->fetch_array("	SELECT *
+												FROM user
+												WHERE user_id = ". $session_info[0]['user_session_uid']);
+				
+				$_SESSION['user_level'] = $user_info[0]['user_level'];
+				$_SESSION['user_name'] = $user_info[0]['user_name'];
+				$_SESSION['user_id'] = $user_info[0]['user_id'];
+				$_SESSION['login'] = true;
+
+				// Since our session expired, we are essentially just logging in again
+				// TODO: remove or the update in user_session, or the update in user,
+				//  kinda doing double work here
+				$db->query("UPDATE user
+							SET user_last_login = ". time() ."
+							WHERE user_id = ". $session_info[0]['user_session_uid']);
+
+				$db->query("UPDATE user_session
+							SET user_session_age = ". time() ."
+							WHERE user_session_id = ". $session_info[0]['user_session_id']);
+			}
+			
+		}else{
+			// Destroy the cookie, since it's invalid
+			cookie_destroy("login_key");
+		}
 	}
 
 ?>
